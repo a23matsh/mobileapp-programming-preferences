406e4d5ac86770a3d3509ce3de8a178d9d00ffa8
a23matsh <a23matsh@student.his.se>
2024-04-29 10:07:40 +0200

    Added a second activity as empty views and cleaned out unecessary code and changed versions, from here the app is running.

---
+++ app/src/main/java/com/example/project/second_activity.java
@@ -0,0 +1,15 @@
+package com.example.project;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+}
