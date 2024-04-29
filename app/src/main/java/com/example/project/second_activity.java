918ccea2d34339aad7d0be24eeeb742819d70667
a23matsh <a23matsh@student.his.se>
2024-04-29 11:27:05 +0200

    Implemented a save button for edittext in the second activity

--- app/src/main/java/com/example/project/second_activity.java
+++ app/src/main/java/com/example/project/second_activity.java
@@ -1,14 +1,25 @@
 package com.example.project;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class second_activity extends AppCompatActivity {
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        final EditText editText = findViewById(R.id.settingseditview);
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
 
 
     }
