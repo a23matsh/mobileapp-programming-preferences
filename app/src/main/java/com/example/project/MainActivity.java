8b325e29b5141f3836e153c7de57b731d05f5703
a23matsh <a23matsh@student.his.se>
2024-04-29 12:05:53 +0200

    Checkpoint save (for myself)

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myPreferenceRef = getSharedPreferences("My prefs", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
         Button buttonToStartSecondActivity = findViewById(R.id.buttonToStartSecondActivity);
         buttonToStartSecondActivity.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +36,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void onResume() {
+        super.onResume();
+        getSharedPreferences("My prefs", MODE_PRIVATE);
+
+    }
+
 }
--- app/src/main/java/com/example/project/second_activity.java
+++ app/src/main/java/com/example/project/second_activity.java
@@ -2,6 +2,7 @@ package com.example.project;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +20,18 @@ public class second_activity extends AppCompatActivity {
 
         final EditText editText = findViewById(R.id.settingseditview);
         Button saveButton = findViewById(R.id.saveButton);
+
+        myPreferenceRef = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
         saveButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                myPreferenceEditor.putString("MyPreferenceString", text);
+                myPreferenceEditor.apply();
 
+            }
+        });
     }
 }
